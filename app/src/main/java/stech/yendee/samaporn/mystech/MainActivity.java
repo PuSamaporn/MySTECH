d9e49591593aa24647378106d012df0d19832e62
samaporn <sama_pu@hotmail.com>
2017-04-04 15:57:28 +0700

    Initial View OK

--- app/src/main/java/stech/yendee/samaporn/mystech/MainActivity.java
+++ app/src/main/java/stech/yendee/samaporn/mystech/MainActivity.java
@@ -2,12 +2,37 @@ package stech.yendee.samaporn.mystech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View  การผูกตัวแปรกับ view ที่อยู่ใน Activity
+        initialView();
+
+
+
+    }    // Main Method
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+
+
     }
-}
+
+}   // Main class นี่คือ class หลัก
