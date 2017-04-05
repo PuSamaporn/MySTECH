38e8663f45818c5bece565dacfa1412dc4ffb190
samaporn <sama_pu@hotmail.com>
2017-04-05 10:40:14 +0700

    Get Event from Click OK

--- app/src/main/java/stech/yendee/samaporn/mystech/MainActivity.java
+++ app/src/main/java/stech/yendee/samaporn/mystech/MainActivity.java
@@ -2,11 +2,14 @@ package stech.yendee.samaporn.mystech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //Explicit
     private EditText userEditText, passwordEditText;
     private TextView textView;
@@ -21,9 +24,18 @@ public class MainActivity extends AppCompatActivity {
         //Initial View  การผูกตัวแปรกับ view ที่อยู่ใน Activity
         initialView();
 
+        //Create Controller
+        controller();
+
 
 
     }    // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+
+    }
 
     private void initialView() {
         userEditText = (EditText) findViewById(R.id.edtUser);
@@ -31,8 +43,27 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.txtRegister);
         button = (Button) findViewById(R.id.btnLogin);
 
-
-
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+
+        String tag = "SriwattanaV1";
+
+        // For TextView   ทำเงื่อนไข
+        if (v == textView) {
+            Log.d(tag, "You Click TextView");
+        }
+
+        //For Button  You click button
+        if (v == button) {
+            Log.d(tag, "You click Button");
+        }
+
+
+
+    }   //onClick
 
 }   // Main class นี่คือ class หลัก
